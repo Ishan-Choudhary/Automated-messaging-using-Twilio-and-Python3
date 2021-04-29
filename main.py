00f6c49a00998c2f54b90b36788e249536c7a593
Ishan-Choudhary <73456920+Ishan-Choudhary@users.noreply.github.com>
2021-04-29 08:53:22 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from twilio.rest import Client
 
-ACCOUNT_SID = ('AC54903fd3e716820edaef9885350d5f1c')
-AUTH_TOKEN = ('8e732e74430934c068b1772dac2ffb68')
+ACCOUNT_SID = ('Your Account SID')
+AUTH_TOKEN = ('Your Authorisation Token')
 
 client = Client(ACCOUNT_SID, AUTH_TOKEN)
 
