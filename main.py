a8acca3c52509f6cbea58ddcfc51dfa5de386212
Ishan-Choudhary <73456920+Ishan-Choudhary@users.noreply.github.com>
2021-10-28 13:26:30 +0530

    Update main.py

--- main.py
+++ main.py
@@ -5,6 +5,6 @@ AUTH_TOKEN = ('Your Authorisation Token')
 
 client = Client(ACCOUNT_SID, AUTH_TOKEN)
 
-client.messages.create(from_='+12513195138',
-                        to='+919920062711',
+client.messages.create(from_='number you recieved',
+                        to='number to message to',
                         body='I have written this through python')
